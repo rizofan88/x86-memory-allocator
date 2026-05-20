5ba18a627a2b5f6f08e94a8b0d278bdbb088de3a
rizofan88 <lorenzoangelini0@gmail.com>
2026-05-20 20:55:55 +0200

    Added constants to linux_defs.s

--- include/constants/linux_defs.s
+++ include/constants/linux_defs.s
@@ -3,7 +3,7 @@
 
 .equ SYS_READ, 3
 .equ SYS_WRITE, 4
-.equ SYS_OPEN,  5
+.equ SYS_OPEN, 5
 .equ SYS_CLOSE, 6
 
 .equ SYS_WAITPID, 7
@@ -11,13 +11,13 @@
 .equ SYS_BRK, 45
 
 .equ SYS_DUP, 41
-.equ SYS_DUP2,  63
+.equ SYS_DUP2, 63
 
-.equ O_WRONLY,  01
-.equ O_CREAT,   0100
-.equ O_TRUNC,   01000
+.equ O_WRONLY, 01
+.equ O_CREAT, 0100
+.equ O_TRUNC, 01000
 
-.equ STDIN,    0
-.equ STDOUT,    1
-.equ STDERR,    2
+.equ STDIN, 0
+.equ STDOUT, 1
+.equ STDERR, 2
 .equ LINUX_SYSCALL, 0x80
